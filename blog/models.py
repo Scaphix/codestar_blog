cbeeb39a5d595c42faac99fe6a400ab0d7b0d67a
Scaphix <chahinez.hoelzenbein@gmail.com>
2025-11-19 12:01:46 +0100

    Update author related name

--- blog/models.py
+++ blog/models.py
@@ -24,7 +24,7 @@ class Comment(models.Model):
         Post, on_delete=models.CASCADE, related_name="comments"
     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="comments"
+        User, on_delete=models.CASCADE, related_name="commenter"
     )
     body = models.TextField()
     approved = models.BooleanField(default=False)
